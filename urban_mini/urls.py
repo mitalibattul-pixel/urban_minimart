49212910b8ab8576dc352601cb356e53d09ff704
Battul Mitali <mitali@gmail.com>
2026-01-19 05:31:20 +0530

    Initial commit

---
+++ urban_mini/urls.py
@@ -0,0 +1,37 @@
+"""
+URL configuration for urban_mini project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
+    path('about/', views.about, name='about'),
+    path('service/', views.service, name='service'),
+    path('faq/', views.faq, name='faq'),
+    path('locations/', views.locations, name='locations'),
+    path('contact/', views.contact, name='contact'),
+    path('shop/', views.shop, name='shop'),
+    path('product_details/', views.product_details, name='product_details'),
+    path('cart/', views.cart, name='cart'),
+    path('wishlist/', views.wishlist, name='wishlist'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('account/', views.account, name='account'),
+]
+
---
+++ urban_mini/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def service(request):
+    return render(request, 'service.html')
+
+def faq(request):
+    return render(request, 'faq.html')
+
+def locations(request):
+    return render(request, 'locations.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def shop(request):
+    return render(request, 'shop.html')
+
+def product_details(request):
+    return render(request, 'product_details.html')
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def wishlist(request):
+    return render(request, 'wishlist.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def account(request):
+    return render(request, 'account.html')
